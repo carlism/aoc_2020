98b2e9f26d23a99c0265af84a70f0272c16f3238
Carl E Leiby <carl@leiby.net>
2020-12-09 18:52:40 -0500

    day9 solution

--- day9/lib/day9.ex
+++ day9/lib/day9.ex
@@ -3,16 +3,86 @@ defmodule Day9 do
   Documentation for Day9.
   """
 
-  @doc """
-  Hello world.
+  def part1 do
+    find_invalid(getXmas("input.txt"), 25) |> Enum.each(&IO.puts/1)
+  end
 
-  ## Examples
+  def part2 do
+    stream = getXmas("input.txt")
+    target = find_invalid(stream, 25) |> List.first()
+    find_contiguous_sum(stream, target) |> add_smallest_to_largest() |> IO.puts()
+  end
 
-      iex> Day9.hello()
-      :world
+  def getXmas(filename) do
+    File.stream!(filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+  end
 
-  """
-  def hello do
-    :world
+  def add_smallest_to_largest(list) do
+    Enum.max(list) + Enum.min(list)
+  end
+
+  def find_contiguous_sum(stream, target, window_start \\ 0, window_length \\ 1) do
+    window = stream |> Stream.drop(window_start) |> Stream.take(window_length)
+    sum = window |> Enum.sum()
+
+    cond do
+      sum == target ->
+        window |> Enum.to_list()
+
+      sum > target ->
+        find_contiguous_sum(stream, target, window_start + 1, window_length - 1)
+
+      sum < target ->
+        find_contiguous_sum(stream, target, window_start, window_length + 1)
+
+      true ->
+        {:error}
+    end
+  end
+
+  def pairs(list) when length(list) < 2 do
+    []
+  end
+
+  def pairs([head | tail]) do
+    tail |> Enum.map(fn element -> {head, element} end) |> Enum.concat(pairs(tail))
+  end
+
+  def check_preamble(queue, value) do
+    if :queue.to_list(queue)
+       |> pairs()
+       |> Enum.map(fn {x, y} -> x + y end)
+       |> Enum.member?(value) do
+      []
+    else
+      [value]
+    end
+  end
+
+  def find_invalid(stream, preamble) do
+    stream
+    |> Stream.with_index()
+    |> Stream.transform(:queue.new(), fn {value, index}, queue ->
+      invalid_values =
+        if index + 1 > preamble do
+          check_preamble(queue, value)
+        else
+          []
+        end
+
+      queue = :queue.in(value, queue)
+
+      queue =
+        if index + 1 > preamble do
+          :queue.out(queue) |> elem(1)
+        else
+          queue
+        end
+
+      {invalid_values, queue}
+    end)
+    |> Enum.to_list()
   end
 end
--- day9/test/day9_test.exs
+++ day9/test/day9_test.exs
@@ -2,7 +2,38 @@ defmodule Day9Test do
   use ExUnit.Case
   doctest Day9
 
-  test "greets the world" do
-    assert Day9.hello() == :world
+  test "pairs" do
+    assert Day9.pairs([1, 2, 3]) == [{1, 2}, {1, 3}, {2, 3}]
+  end
+
+  test "check preamble" do
+    preamble = :queue.from_list([1, 2, 3])
+    assert Day9.check_preamble(preamble, 1) == [1]
+    assert Day9.check_preamble(preamble, 2) == [2]
+    assert Day9.check_preamble(preamble, 3) == []
+    assert Day9.check_preamble(preamble, 4) == []
+    assert Day9.check_preamble(preamble, 5) == []
+    assert Day9.check_preamble(preamble, 6) == [6]
+    assert Day9.check_preamble(preamble, 346) == [346]
+  end
+
+  test "solution" do
+    assert Day9.find_invalid(Day9.getXmas("test/input.txt"), 5) == [127]
+  end
+
+  test "idea" do
+    str = Stream.iterate(1, fn e -> e + 1 end)
+    assert str |> Stream.take(3) |> Enum.to_list() == [1, 2, 3]
+    assert str |> Stream.take(3) |> Enum.to_list() == [1, 2, 3]
+    assert str |> Stream.drop(3) |> Stream.take(3) |> Enum.to_list() == [4, 5, 6]
+    str = Day9.getXmas("test/input.txt")
+    assert str |> Stream.take(3) |> Enum.to_list() == [35, 20, 15]
+    assert str |> Stream.drop(3) |> Stream.take(3) |> Enum.to_list() == [25, 47, 40]
+  end
+
+  test "find 127" do
+    str = Day9.getXmas("test/input.txt")
+    assert Day9.find_contiguous_sum(str, 127) == [15, 25, 47, 40]
+    assert Day9.add_smallest_to_largest([15, 25, 47, 40]) == 62
   end
 end
